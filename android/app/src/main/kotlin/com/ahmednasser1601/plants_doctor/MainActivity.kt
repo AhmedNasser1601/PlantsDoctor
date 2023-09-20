3df11a6a00f7e32e49645522f60181ca6cca4f5d
Ahmed Nasser <ahmednasser1601@gmail.com>
2023-09-21 00:42:22 +0300

    Android

---
+++ android/app/src/main/kotlin/com/ahmednasser1601/plants_doctor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahmednasser1601.plants_doctor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/ahmednasser1601/plantsdoctor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahmednasser1601.plantsdoctor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
